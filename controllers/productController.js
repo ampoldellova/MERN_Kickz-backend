daa920572bddff9a75dcedb6f93865a8bb9d4b99
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-10-25 20:13:04 +0800

    Get Product by ID

--- controllers/productController.js
+++ controllers/productController.js
@@ -28,3 +28,17 @@ exports.getProducts = async (req,res,next) => {
 		resPerPage,
 	})
 }
+
+exports.getSingleProduct = async (req, res, next) => {
+	const product = await Product.findById(req.params.id);
+	if (!product) {
+		return res.status(404).json({
+			success: false,
+			message: 'Product not found'
+		})
+	}
+	res.status(200).json({
+		success: true,
+		product
+	})
+}
