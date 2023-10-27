26b38f71050bf9f3dee6499fcbfc24960da8624e
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-10-28 01:47:11 +0800

    Register User API

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const products = require('./routes/product');
+const auth = require('./routes/auth');
 
 app.use(cors())
-app.use(express.json());
-app.use(express.urlencoded({limit: "100mb", extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cookieParser());
 
 app.use('/api/v1', products);
+app.use('/api/v1', auth);
 
 module.exports = app
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,27 +1,25 @@
 const User = require('../models/user');
 const sendToken = require('../utils/jwtToken');
-const cloudinary = require('cloudinary')
-const crypto = require('crypto')
 
 exports.registerUser = async (req, res, next) => {
-    const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
-        folder: 'avatars',
-        width: 150,
-        crop: "scale"
-    }, (err, res) => {
-        console.log(err, res);
-    });
+    // const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
+    //     folder: 'avatars',
+    //     width: 150,
+    //     crop: "scale"
+    // }, (err, res) => {
+    //     console.log(err, res);
+    // });
     const { name, email, password, role } = req.body;
     const user = await User.create({
         name,
         email,
         password,
-        avatar: {
-            public_id: result.public_id,
-            url: result.secure_url
-        },
+        // avatar: {
+        //     public_id: result.public_id,
+        //     url: result.secure_url
+        // },
 
-        // role,
+        role
     })
 
     // const token = user.getJwtToken();
@@ -32,5 +30,4 @@ exports.registerUser = async (req, res, next) => {
         })
     }
     sendToken(user, 200, res)
-
 }
