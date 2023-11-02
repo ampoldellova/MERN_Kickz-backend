81ffe724d02b8118e77eed1860dd0c47754f21ba
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-11-02 16:37:41 +0800

    Update User Profile

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const upload = require("../utils/multer");
 
-const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserProfile, updatePassword } = require('../controllers/authController');
+const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserProfile, updatePassword, updateProfile } = require('../controllers/authController');
 const { isAuthenticatedUser } = require('../middlewares/auth');
 
 router.post('/register', upload.single("avatar"), registerUser);
@@ -12,5 +12,6 @@ router.post('/password/forgot', forgotPassword);
 router.put('/password/reset/:token', resetPassword);
 router.get('/me', isAuthenticatedUser, getUserProfile)
 router.put('/password/update', isAuthenticatedUser, updatePassword)
+router.put('/me/update', isAuthenticatedUser, upload.single("avatar"), updateProfile)
 
 module.exports = router;
