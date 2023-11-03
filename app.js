67cd489a7ce6a0b35f022bd5758d3d300dd45f09
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-11-03 23:57:26 +0800

    Order (Controller, Routes, Models)

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 
 const products = require('./routes/product');
 const auth = require('./routes/auth');
+const order = require('./routes/order');
 
 app.use(cors())
 app.use(express.json({ limit: '50mb' }));
@@ -13,5 +14,6 @@ app.use(cookieParser());
 
 app.use('/api/v1', products);
 app.use('/api/v1', auth);
+app.use('/api/v1', order);
 
 module.exports = app
