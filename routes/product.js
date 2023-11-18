2bf327ab9035b5d726bf6fd69a2ff87600035277
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-11-18 20:35:34 +0800

    Admin Product Lists

--- routes/product.js
+++ routes/product.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct, getAdminProducts } = require('../controllers/productController');
-const { isAuthenticatedUser } = require('../middlewares/auth');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 router.get('/products', getProducts);
 router.post('/product/new', newProduct);
 router.get('/product/:id', getSingleProduct);
-router.route('/admin/product/:id', isAuthenticatedUser).put(updateProduct).delete(deleteProduct);
+router.route('/admin/product/:id', isAuthenticatedUser, authorizeRoles('admin') ).put(updateProduct).delete(deleteProduct);
 router.get('/admin/products', isAuthenticatedUser, authorizeRoles('admin'), getAdminProducts);
 
 module.exports = router;
