24c30734abd1ec022d3f6a9a2b702d0a7bf62549
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-11-19 09:43:32 +0800

    Create Product

--- routes/product.js
+++ routes/product.js
@@ -6,9 +6,9 @@ const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct,
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 router.get('/products', getProducts);
-router.post('/product/new', newProduct);
 router.get('/product/:id', getSingleProduct);
-router.route('/admin/product/:id', isAuthenticatedUser, authorizeRoles('admin') ).put(updateProduct).delete(deleteProduct);
+router.post('/admin/product/new', isAuthenticatedUser, authorizeRoles('admin',), upload.array('images', 10), newProduct);
+router.route('/admin/product/:id', isAuthenticatedUser, authorizeRoles('admin')).put(updateProduct).delete(deleteProduct);
 router.get('/admin/products', isAuthenticatedUser, authorizeRoles('admin'), getAdminProducts);
 
 module.exports = router;
