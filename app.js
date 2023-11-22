1f9ab5b2aa6a076c8473ffae4c9f51215ba87f6f
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-11-22 22:16:30 +0800

    Brand Route Link

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const products = require('./routes/product');
 const auth = require('./routes/auth');
 const order = require('./routes/order');
 const supplier = require('./routes/supplier');
+const brand = require('./routes/brand');
 
 app.use(cors())
 app.use(express.json({ limit: '50mb' }));
@@ -17,5 +18,6 @@ app.use('/api/v1', products);
 app.use('/api/v1', auth);
 app.use('/api/v1', order);
 app.use('/api/v1', supplier);
+app.use('/api/v1', brand);
 
 module.exports = app
