0df4eac0d2dc45d379b5b4acee790400ff09f0b0
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-11-22 14:31:30 +0800

    Update Product Route

--- routes/product.js
+++ routes/product.js
@@ -8,7 +8,8 @@ const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 router.get('/products', getProducts);
 router.get('/product/:id', getSingleProduct);
 router.post('/admin/product/new', isAuthenticatedUser, authorizeRoles('admin',), upload.array('images', 10), newProduct);
-router.route('/admin/product/:id', isAuthenticatedUser, authorizeRoles('admin')).put(updateProduct).delete(deleteProduct);
+router.route('/admin/product/:id', isAuthenticatedUser, authorizeRoles('admin')).delete(deleteProduct);
+router.put('/admin/product/:id', isAuthenticatedUser, authorizeRoles('admin'), upload.array('images', 10), updateProduct)
 router.get('/admin/products', isAuthenticatedUser, authorizeRoles('admin'), getAdminProducts);
 
 module.exports = router;
